cccfaa6903ee665ace78977e52c158f44a1ce8f1
Richard Romero <qricardo@outlook.com>
2022-05-30 09:42:32 -0500

    Added schemas

--- Library/Exchanges/BinanceAPI/src/schemas.cpp
+++ Library/Exchanges/BinanceAPI/src/schemas.cpp
@@ -82,79 +82,84 @@ namespace BINAPI_NAMESPACE::local::schemas
 "required":["data","type","updateTime"]}],"additionalItems":false}},"additionalProperties":false,"required":["code","msg","snapshotVos"]})" };
         char *disable_fast_withdraw_switch   = empty_schema;
         char *enable_fast_withdraw_switch    = empty_schema;
-        char withdraw[]                      = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"id":{"type":"string"}},"additionalProperties":false,"required":["id"]})"};
+        char withdraw[]                      = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"id":{"type":"string"}},"additionalProperties":false,"required":["id"]})" };
         char deposit_history[]               = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"array","items":{"type":"object","properties":{"amount":{"type":"string"},
 "coin":{"type":"string"},"network":{"type":"string"},"status":{"type":"integer"},"address":{"type":"string"},"addressTag":{"type":"string"},"txId":{"type":"string"},"insertTime":
 {"type":"integer"},"transferType":{"type":"integer"},"unlockConfirm":{"type":"string"},"confirmTimes":{"type":"string"}},"additionalProperties":false,"required":
-["amount","coin","network","status","address","addressTag","txId","insertTime","transferType","unlockConfirm","confirmTimes"]},"additionalItems":false})"};
+["amount","coin","network","status","address","addressTag","txId","insertTime","transferType","unlockConfirm","confirmTimes"]},"additionalItems":false})" };
         char withdraw_history[]              = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"array","items":{"type":"object","properties":{"address":{"type":"string"},"amount":
 {"type":"string"},"applyTime":{"type":"string"},"coin":{"type":"string"},"id":{"type":"string"},"withdrawOrderId":{"type":"string"},"network":{"type":"string"},"transferType":{"type":"integer"},
 "status":{"type":"integer"},"transactionFee":{"type":"string"},"confirmNo":{"type":"integer"},"info":{"type":"string"},"txId":{"type":"string"}},"additionalProperties":false,"required":["address",
-"amount","applyTime","coin","id","network","transferType","status","transactionFee","confirmNo","info","txId"]},"additionalItems":false})"};
+"amount","applyTime","coin","id","network","transferType","status","transactionFee","confirmNo","info","txId"]},"additionalItems":false})" };
         char deposit_address[]               = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"address":{"type":"string"},"coin":{"type":"string"},"tag"
-:{"type":"string"},"url":{"type":"string"}},"additionalProperties":false,"required":["address","coin","tag","url"]})"};
-        char account_status[]                = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"data":{"type":"string"}},"additionalProperties":false,"required":["data"]})"};
+:{"type":"string"},"url":{"type":"string"}},"additionalProperties":false,"required":["address","coin","tag","url"]})" };
+        char account_status[]                = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"data":{"type":"string"}},"additionalProperties":false,"required":["data"]})" };
         char account_api_trading_status[]    = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"data":{"type":"object","properties":{"isLocked":{"type":"boolean"}
 ,"plannedRecoverTime":{"type":"integer"},"triggerCondition":{"type":"object","properties":{"GCR":{"type":"integer"},"IFER":{"type":"integer"},"UFR":{"type":"integer"}},"additionalProperties":false,"required":
-["GCR","IFER","UFR"]},"updateTime":{"type":"integer"}},"additionalProperties":false,"required":["isLocked","plannedRecoverTime","triggerCondition","updateTime"]}},"additionalProperties":false,"required":["data"]})"};
+["GCR","IFER","UFR"]},"updateTime":{"type":"integer"}},"additionalProperties":false,"required":["isLocked","plannedRecoverTime","triggerCondition","updateTime"]}},"additionalProperties":false,"required":["data"]})" };
         char dustlog[]                       = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"total":{"type":"integer"},"userAssetDribblets":{"type":"array",
 "items":{"type":"object","properties":{"operateTime":{"type":"integer"},"totalTransferedAmount":{"type":"string"},"totalServiceChargeAmount":{"type":"string"},"transId":{"type":"integer"},
 "userAssetDribbletDetails":{"type":"array","items":{"type":"object","properties":{"transId":{"type":"integer"},"serviceChargeAmount":{"type":"string"},"amount":{"type":"string"},"operateTime":
 {"type":"integer"},"transferedAmount":{"type":"string"},"fromAsset":{"type":"string"}},"additionalProperties":false,"required":["transId","serviceChargeAmount","amount","operateTime","transferedAmount","fromAsset"]},
 "additionalItems":false}},"additionalProperties":false,"required":["operateTime","totalTransferedAmount","totalServiceChargeAmount","transId","userAssetDribbletDetails"]},"additionalItems":false}},
-"additionalProperties":false,"required":["total","userAssetDribblets"]})"};
+"additionalProperties":false,"required":["total","userAssetDribblets"]})" };
         char assets_convert_to_bnb[]         = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"details":{"type":"array","items":{"type":"object","properties":
 {"asset":{"type":"string"},"assetFullName":{"type":"string"},"amountFree":{"type":"string"},"toBTC":{"type":"string"},"toBNB":{"type":"string"},"toBNBOffExchange":{"type":"string"},"exchange":
 {"type":"string"}},"additionalProperties":false,"required":["asset","assetFullName","amountFree","toBTC","toBNB","toBNBOffExchange","exchange"]},"additionalItems":false},"totalTransferBtc":
-{"type":"string"},"totalTransferBNB":{"type":"string"},"dribbletPercentage":{"type":"string"}},"additionalProperties":false,"required":["details","totalTransferBtc","totalTransferBNB","dribbletPercentage"]})"};
+{"type":"string"},"totalTransferBNB":{"type":"string"},"dribbletPercentage":{"type":"string"}},"additionalProperties":false,"required":["details","totalTransferBtc","totalTransferBNB","dribbletPercentage"]})" };
         char dust_transfer[]                 = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"totalServiceCharge":{"type":"string"},"totalTransfered":
 {"type":"string"},"transferResult":{"type":"array","items":{"type":"object","properties":{"amount":{"type":"string"},"fromAsset":{"type":"string"},"operateTime":{"type":"integer"},
 "serviceChargeAmount":{"type":"string"},"tranId":{"type":"integer"},"transferedAmount":{"type":"string"}},"additionalProperties":false,"required":["amount","fromAsset","operateTime",
-"serviceChargeAmount","tranId","transferedAmount"]},"additionalItems":false}},"additionalProperties":false,"required":["totalServiceCharge","totalTransfered","transferResult"]})"};
+"serviceChargeAmount","tranId","transferedAmount"]},"additionalItems":false}},"additionalProperties":false,"required":["totalServiceCharge","totalTransfered","transferResult"]})" };
         char asset_dividend_record[]         = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"rows":{"type":"array","items":{"type":"object","properties":{"id":
 {"type":"integer"},"amount":{"type":"string"},"asset":{"type":"string"},"divTime":{"type":"integer"},"enInfo":{"type":"string"},"tranId":{"type":"integer"}},"additionalProperties":false,
-"required":["id","amount","asset","divTime","enInfo","tranId"]},"additionalItems":false},"total":{"type":"integer"}},"additionalProperties":false,"required":["rows","total"]})"};
+"required":["id","amount","asset","divTime","enInfo","tranId"]},"additionalItems":false},"total":{"type":"integer"}},"additionalProperties":false,"required":["rows","total"]})" };
         char asset_detail[]                  = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"CTR":{"type":"object","properties":
 {"minWithdrawAmount":{"type":"string"},"depositStatus":{"type":"boolean"},"withdrawFee":{"type":"integer"},"withdrawStatus":{"type":"boolean"},"depositTip":{"type":"string"}},
 "additionalProperties":false,"required":["minWithdrawAmount","depositStatus","withdrawFee","withdrawStatus","depositTip"]},"SKY":{"type":"object","properties":{"minWithdrawAmount":
 {"type":"string"},"depositStatus":{"type":"boolean"},"withdrawFee":{"type":"number"},"withdrawStatus":{"type":"boolean"}},"additionalProperties":false,"required":["minWithdrawAmount",
-"depositStatus","withdrawFee","withdrawStatus"]}},"additionalProperties":false,"required":["CTR","SKY"]})"};
+"depositStatus","withdrawFee","withdrawStatus"]}},"additionalProperties":false,"required":["CTR","SKY"]})" };
         char trade_fee[]                     = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"array","items":{"type":"object","properties":{"symbol":{"type":"string"},
-"makerCommission":{"type":"string"},"takerCommission":{"type":"string"}},"additionalProperties":false,"required":["symbol","makerCommission","takerCommission"]},"additionalItems":false})"};
+"makerCommission":{"type":"string"},"takerCommission":{"type":"string"}},"additionalProperties":false,"required":["symbol","makerCommission","takerCommission"]},"additionalItems":false})" };
         char user_universal_transfer[]       = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"tranId":{"type":"integer"}},
-"additionalProperties":false,"required":["tranId"]})"};
+"additionalProperties":false,"required":["tranId"]})" };
         char query_user_universal_transfer[] = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"total":{"type":"integer"},"rows":
-{"type":"array","items":{"type":"object","properties":{"asset":{"type":"string"},"amount":{"type":"string"},"type":{"type":"string"},"status":{"type":"string"},"tranId":{"type":"integer"},"timestamp":{"type":"integer"}},"additionalProperties":false,"required":["asset","amount","type","status","tranId","timestamp"]},"additionalItems":false}},"additionalProperties":false,"required":["total","rows"]})"};
+{"type":"array","items":{"type":"object","properties":{"asset":{"type":"string"},"amount":{"type":"string"},"type":{"type":"string"},"status":{"type":"string"},"tranId":{"type":"integer"},"timestamp":{"type":"integer"}},"additionalProperties":false,"required":["asset","amount","type","status","tranId","timestamp"]},"additionalItems":false}},"additionalProperties":false,"required":["total","rows"]})" };
         char funding_wallet[]                = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"array","items":{"type":"object","properties":{"asset":
 {"type":"string"},"free":{"type":"string"},"locked":{"type":"string"},"freeze":{"type":"string"},"withdrawing":{"type":"string"},"btcValuation":{"type":"string"}},"additionalProperties":
-false,"required":["asset","free","locked","freeze","withdrawing","btcValuation"]},"additionalItems":false})"};
+false,"required":["asset","free","locked","freeze","withdrawing","btcValuation"]},"additionalItems":false})" };
         char get_api_key_permissions[]       = { R"({"$schema":"http://json-schema.org/draft-04/schema#","type":"object","properties":{"ipRestrict":{"type":"boolean"},
 "createTime":{"type":"integer"},"enableWithdrawals":{"type":"boolean"},"enableInternalTransfer":{"type":"boolean"},"permitsUniversalTransfer":{"type":"boolean"},"enableVanillaOptions":
 {"type":"boolean"},"enableReading":{"type":"boolean"},"enableFutures":{"type":"boolean"},"enableMargin":{"type":"boolean"},"enableSpotAndMarginTrading":{"type":"boolean"},
 "tradingAuthorityExpirationTime":{"type":"integer"}},"additionalProperties":false,"required":["ipRestrict","createTime","enableWithdrawals","enableInternalTransfer",
 "permitsUniversalTransfer","enableVanillaOptions","enableReading","enableFutures","enableMargin","enableSpotAndMarginTrading","tradingAuthorityExpirationTime"]})" };
+        char *test_connectivity              = empty_schema;
+        char check_server_time[]             = { R"({"$schema": "http://json-schema.org/draft-04/schema#","type": "object","properties": { "serverTime": { "type": "integer" }},"required": ["serverTime"]})" };
+        char exchange_info[]                 = { R"({"$schema": "http://json-schema.org/draft-04/schema#","type": "object","properties": {  "timezone": {"type": "string"  }, "serverTime":
+{"type": "integer"  },  "rateLimits": {"type": "array","items": {"type": "object","properties": {"rateLimitType": { "type": "string"},"interval": { "type": "string"},"intervalNum": { "type":
+"integer"},"limit": { "type": "integer"}},"additionalProperties": true,"required": ["rateLimitType","interval","intervalNum","limit"]},"additionalItems": true  },  "exchangeFilters": {"type":
+ "array","items": {"items": {},"additionalItems": true,"additionalProperties": true},"additionalItems": true  },  "symbols": {"type": "array","items": {"type": "object","properties": {"symbol":
+{ "type": "string"},"status": { "type": "string", "enum": [  "TRADING",  "BREAK" ]},"baseAsset": { "type": "string"},"baseAssetPrecision": { "type": "integer"},"quoteAsset": { "type": "string"},
+"quotePrecision": { "type": "integer"},"quoteAssetPrecision": { "type": "integer"},"baseCommissionPrecision": { "type": "integer"},"quoteCommissionPrecision": { "type": "integer"},"orderTypes":
+{ "type": "array", "items": {  "type": "string" }, "additionalItems": true},"icebergAllowed": { "type": "boolean"},"ocoAllowed": { "type": "boolean"},"quoteOrderQtyMarketAllowed": { "type": "boolean"},
+"allowTrailingStop": { "type": "boolean"},"isSpotTradingAllowed": { "type": "boolean"},"isMarginTradingAllowed": { "type": "boolean"},"filters": { "type": "array", "items": {  "type": "object",
+"properties": {"filterType": {"type": "string"},"minPrice": {"type": "string"},"maxPrice": {"type": "string"},"tickSize": {"type": "string"},"multiplierUp": {"type": "string"},"multiplierDown":
+{"type": "string"},"avgPriceMins": {"type": "integer"},"minQty": {"type": "string"},"maxQty": {"type": "string"},"stepSize": {"type": "string"},"minNotional": {"type": "string"},"applyToMarket":
+ {"type": "boolean"},"limit": {"type": "integer"},"minTrailingAboveDelta": {"type": "integer"},"maxTrailingAboveDelta": {"type": "integer"},"minTrailingBelowDelta": {"type": "integer"},
+"maxTrailingBelowDelta": {"type": "integer"},"maxNumOrders": {"type": "integer"},"maxNumAlgoOrders": {"type": "integer"}  },  "additionalProperties": true,  "required": ["filterType"  ] },
+"additionalItems": true},"permissions": { "type": "array", "items": {  "type": "string" }, "additionalItems": true}},"additionalProperties": true,"required": ["symbol","status","baseAsset",
+"baseAssetPrecision","quoteAsset","quotePrecision","quoteAssetPrecision","baseCommissionPrecision","quoteCommissionPrecision","orderTypes","icebergAllowed","ocoAllowed","quoteOrderQtyMarketAllowed",
+"allowTrailingStop","isSpotTradingAllowed","isMarginTradingAllowed","filters","permissions"]},"additionalItems": true  }},"additionalProperties": true,"required": [  "timezone",  "serverTime",
+ "rateLimits",  "exchangeFilters",  "symbols"]})" };
+        char order_book[] = {R"({"$schema": "http://json-schema.org/draft-07/schema","$id": "http://richardqpc.com/schema/order_book_ticker.json","type": "object","required": [ "lastUpdateId", "bids",
+ "asks"],"properties": { "lastUpdateId": { "$id": "#/properties/lastUpdateId", "type": "integer" }, "bids": { "$id": "#/properties/bids", "type": "array", "additionalItems": true, "items": { "$id":
+"#/properties/bids/items", "anyOf": [ { "$id": "#/properties/bids/items/anyOf/0", "type": "array", "additionalItems": true, "items": { "$id": "#/properties/bids/items/anyOf/0/items", "anyOf":
+ [ { "$id": "#/properties/bids/items/anyOf/0/items/anyOf/0", "type": "string" } ] } } ] } }, "asks": { "$id": "#/properties/asks", "type": "array", "additionalItems": true, "items":
+ { "$id": "#/properties/asks/items", "anyOf": [ { "$id": "#/properties/asks/items/anyOf/0", "type": "array", "additionalItems": true, "items": { "$id": "#/properties/asks/items/anyOf/0/items",
+"anyOf": [ { "$id": "#/properties/asks/items/anyOf/0/items/anyOf/0", "type": "string" } ] } } ] } }},"additionalProperties": false})"};
     } // namespace SPOT
     namespace Futures
     {
-        char exchange_info[] = {
-            R"({"$schema": "http://json-schema.org/draft-04/schema#",
-"type": "object", "properties": { "timezone": { "type": "string" }, "serverTime": { "type": "integer" }, "futuresType": { "type": "string" },
-"rateLimits": { "type": "array", "items": {"type": "object", "properties": { "rateLimitType": { "type": "string" }, "interval": { "type": "string" }, "intervalNum": { "type": "integer" },
-"limit": { "type": "integer" } }, "required": ["rateLimitType", "interval", "intervalNum", "limit"] } },"exchangeFilters": { "type": "array", "items": { "items": {}  } },
-"assets": { "type": "array", "items": { "type": "object", "properties": { "asset": { "type": "string" }, "marginAvailable": { "type": "boolean" },"autoAssetExchange": { "type": "string" }  },
-"required": [ "asset", "marginAvailable", "autoAssetExchange" ] }  },   "symbols": { "type": "array", "items": { "type": "object", "properties": { "symbol": { "type": "string" }, "pair": { "type": "string" },
-"contractType": { "type": "string" }, "deliveryDate": { "type": "integer" }, "onboardDate": { "type": "integer" }, "status": { "type": "string" },
-"maintMarginPercent": { "type": "string" }, "requiredMarginPercent": {"type": "string" },"baseAsset": { "type": "string" }, "quoteAsset": { "type": "string" },"marginAsset": { "type": "string" },
-"pricePrecision": { "type": "integer" }, "quantityPrecision": {"type": "integer" }, "baseAssetPrecision": { "type": "integer" }, "quotePrecision": { "type": "integer" }, "underlyingType": { "type": "string" },
-"underlyingSubType": { "type": "array", "items": { "type": "string" }},"settlePlan": {"type": "integer" }, "triggerProtect": { "type": "string" }, "liquidationFee": {"type": "string"},"marketTakeBound": { "type": "string" },
-"filters": { "type": "array", "items": {"type": "object","properties": { "minPrice": { "type": "string"},"maxPrice": { "type": "string" },
-"filterType": { "type": "string" },"tickSize": { "type": "string" }, "stepSize": {"type": "string"},"maxQty": { "type": "string" },"minQty": { "type": "string" },
-"limit": { "type": "integer"}, "notional": { "type": "string"}, "multiplierDown": {"type": "string" }, "multiplierUp": {"type": "string" },
-"multiplierDecimal": {"type": "string" } }, "required": ["filterType"]}}, "orderTypes": {"type": "array", "items": {"type": "string" } }, "timeInForce": {"type": "array","items": {"type": "string" } } },
-"required": ["symbol","pair","contractType","deliveryDate","onboardDate","status", "maintMarginPercent","requiredMarginPercent","baseAsset", "quoteAsset","marginAsset","pricePrecision","quantityPrecision",
-"baseAssetPrecision", "quotePrecision", "underlyingType", "underlyingSubType", "settlePlan", "triggerProtect", "liquidationFee", "marketTakeBound", "filters", "orderTypes", "timeInForce"]  }}},
-"required": [ "timezone","serverTime","futuresType", "rateLimits","exchangeFilters", "assets", "symbols"]})"
-        };
 
         char query_order[] = {
             R"({"$schema": "http://json-schema.org/draft-04/schema#",
