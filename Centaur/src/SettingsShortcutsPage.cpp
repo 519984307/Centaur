cc1620f989acba8fc3d8fd9975554ad3c76515c3
Richard Romero <qricardo@outlook.com>
2023-02-13 15:56:44 -0600

    Move the settings page to different source files. This behavior will probably change in the future to separate the pages into different widget classes

---
+++ Centaur/src/SettingsShortcutsPage.cpp
@@ -0,0 +1,16 @@
+/////////////////////////////////////////////////////////////////////////////////////
+//
+// Created by Ricardo Romero on 13/02/23.
+// Copyright (c) 2023 Ricardo Romero.  All rights reserved.
+//
+
+#include "../ui/ui_SettingsDialog.h"
+#include "SettingsDialog.hpp"
+
+BEGIN_CENTAUR_NAMESPACE
+
+void SettingsDialog::initShortcutsWidget() noexcept
+{
+}
+
+END_CENTAUR_NAMESPACE
